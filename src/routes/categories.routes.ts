de6a24ac08b36a6ff7add82f26a986ca161e95da
Lucca Scarano <scarano.dev@gmail.com>
2021-08-04 11:19:20 -0300

    add quick fail check

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -7,6 +7,11 @@ const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/categories', (request: Request, response: Response) => {
 	const { name, description } = request.body;
+	const categoryAlreadyExists = categoriesRepository.findByName(name);
+
+	if (categoryAlreadyExists) {
+		return response.status(400).json({ error: 'Category already exists!' });
+	}
 
 	categoriesRepository.create({ name, description });
 
