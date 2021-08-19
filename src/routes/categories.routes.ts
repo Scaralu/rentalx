eca1d9bc345328bb88ea60622de8ac5a76be2226
Lucca Scarano <scarano.dev@gmail.com>
2021-08-19 15:49:40 -0300

    dependency inversion principle

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,12 +1,12 @@
 import { Request, Response, Router } from 'express';
 
-import { CategoriesRepository } from '../repositories/CategoriesRepository';
-import { CreateCategoryService } from '../services/CreateCategoryService';
+import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository';
+import { CreateCategoryService } from '../modules/cars/services/CreateCategoryService';
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
-categoriesRoutes.post('/categories', (request: Request, response: Response) => {
+categoriesRoutes.post('/', (request: Request, response: Response) => {
 	const { name, description } = request.body;
 	const createCategoryService = new CreateCategoryService(categoriesRepository);
 
@@ -15,7 +15,7 @@ categoriesRoutes.post('/categories', (request: Request, response: Response) => {
 	return response.status(201).send();
 });
 
-categoriesRoutes.get('/categories', (request: Request, response: Response) => {
+categoriesRoutes.get('/', (_request: Request, response: Response) => {
 	const all = categoriesRepository.list();
 
 	return response.status(200).json(all);
