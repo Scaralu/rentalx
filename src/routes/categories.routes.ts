42cf762e3fa5544b3358365af5be735b9ce8cf44
Lucca Scarano <scarano.dev@gmail.com>
2021-09-14 09:45:51 -0300

    accept file with multer

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,16 +1,26 @@
-import { Request, Response, Router } from 'express';
+import { Router } from 'express';
+import multer from 'multer';
 
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
+import { importCategoriesController } from '../modules/cars/useCases/importCategories';
 import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 
 const categoriesRoutes = Router();
 
-categoriesRoutes.post('/', (request: Request, response: Response) =>
+const upload = multer({
+	dest: './tmp',
+});
+
+categoriesRoutes.post('/', (request, response) =>
 	createCategoryController.handle(request, response)
 );
 
-categoriesRoutes.get('/', (request: Request, response: Response) =>
+categoriesRoutes.get('/', (request, response) =>
 	listCategoriesController.handle(request, response)
 );
+
+categoriesRoutes.post('/import', upload.single('file'), (request, response) => {
+	importCategoriesController.handle(request, response);
+});
 
 export { categoriesRoutes };
